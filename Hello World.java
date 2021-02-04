a50268c7c3086b4080d958ebdfbb481e0b3750f4
fanboyan <812008450@qq.com>
2021-02-04 16:14:29 +0800

    HelloWorld

---
+++ Hello World.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    /* 第一个Java程序
+     * 它将输出字符串 Hello World
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // 输出 Hello World
+        System.out.println("1"); // 输出 Hello World
+        System.out.println("Hello World"); // 输出 Hello World
+        System.out.println("2"); // 输出 Hello World
+    }
+}
